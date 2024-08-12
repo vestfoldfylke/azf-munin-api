4731fda86746ff7778bd8a00cfd595dbf118bd66
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-12 12:55:48 +0200

    Refactor OpenAI assistantOpenAi.js to use params object instead of parsing request body

--- src/functions/multimodalOpenAi.js
+++ src/functions/multimodalOpenAi.js
@@ -1,6 +1,7 @@
 const { app } = require("@azure/functions");
 const { OpenAI } = require("openai");
-require("dotenv").config();
+const validateToken = require("../lib/validateToken");
+// require("dotenv").config();
 
 app.http("multimodalOpenAi", {
   methods: ["GET", "POST"],
@@ -8,44 +9,50 @@ app.http("multimodalOpenAi", {
   handler: async (request, context) => {
     const openai = new OpenAI();
     const params = await JSON.parse(await request.text());
+    const accesstoken = request.headers.get("Authorization");
 
-    console.log(params);
+    try {
+      // Validate the token and the role of the user
+      await validateToken(accesstoken, { role: ["hugin.basic", "hugin.admin"] })  
 
-    msg = [{ role: "system", content: params.kontekst }];
-    msg.push(...params.messageHistory);
+      msg = [{ role: "system", content: params.kontekst }];
+      msg.push(...params.messageHistory);
 
-    if (params.bilde_base64String !== "") {
-      console.log("Bilde er definert");
-      msg.push({
-        role: "user",
-        content: [
-          { type: "text", text: params.message },
-          {
-            type: "image_url",
-            image_url: {
-              url: params.bilde_base64String,
+      if (params.bilde_base64String !== "") {
+        msg.push({
+          role: "user",
+          content: [
+            { type: "text", text: params.message },
+            {
+              type: "image_url",
+              image_url: {
+                url: params.bilde_base64String,
+              },
             },
-          },
-        ],
+          ],
+        });
+      } else {
+        msg.push({ role: "user", content: params.message });
+      }
+
+      //const moderation = await openai.moderations.create({ input: "I want to kill them." });
+      //console.log(params.msg);
+      //console.log(JSON.stringify(moderation));
+
+      const completion = await openai.chat.completions.create({
+        messages: msg,
+        model: params.model,
+        temperature: params.temperature,
       });
-    } else {
-      console.log("Bilde er ikke definert");
-      msg.push({ role: "user", content: params.message });
+
+      return {
+        body: JSON.stringify(completion),
+      };
+    } catch (error) {
+      return {
+        status: 401,
+        body: JSON.stringify({ error: error.message }),
+      }
     }
-
-    //const moderation = await openai.moderations.create({ input: "I want to kill them." });
-    //console.log(params.msg);
-    //console.log(JSON.stringify(moderation));
-
-    const completion = await openai.chat.completions.create({
-      messages: msg,
-      model: params.model,
-      temperature: params.temperature,
-    });
-
-    console.log(completion.choices[0]);
-    return {
-      body: JSON.stringify(completion),
-    };
   },
 });
