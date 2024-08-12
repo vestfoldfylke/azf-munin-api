910768ac03b8262e3b28abd84c0c551c027aeaa8
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-12 12:55:38 +0200

    chore: Refactor OpenAI assistantOpenAi.js to use params object instead of parsing request body

--- src/functions/assistantOpenAi.js
+++ src/functions/assistantOpenAi.js
@@ -1,62 +1,72 @@
 const { app } = require("@azure/functions");
 const { OpenAI } = require("openai");
-require("dotenv").config();
+const validateToken = require("../lib/validateToken");
+// require("dotenv").config();
 
 app.http('assistantOpenAi', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-      const openai = new OpenAI();
-      const params = JSON.parse(await request.text());
+      try {
+        const accesstoken = request.headers.get("Authorization");
+        await validateToken(accesstoken, { role: ["hugin.basic", "hugin.admin"] });
+        const openai = new OpenAI();
+        const params = JSON.parse(await request.text());
 
-      // Sjekker om det skal opprettes en ny tråd eller om det skal brukes en eksisterende
-      let thread;
-      if (params.new_thread) {
-        thread = await openai.beta.threads.create();
-      } else {
-        thread = await openai.beta.threads.retrieve(params.thread_id);
-      }
-
-      // Legger til brukerens spørsmål i tråden
-      const message = await openai.beta.threads.messages.create(
-        thread.id,
-        {
-          role: "user",
-          content: params.question
+        // Sjekker om det skal opprettes en ny tråd eller om det skal brukes en eksisterende
+        let thread;
+        if (params.new_thread) {
+          thread = await openai.beta.threads.create();
+        } else {
+          thread = await openai.beta.threads.retrieve(params.thread_id);
         }
-      );
 
-      // Kjører tråden på valgt assistent
-      const run = await openai.beta.threads.runs.createAndPoll(
-        thread.id,
-        {
-          assistant_id: params.assistant_id,
-        },
-      );
-      console.log("Ass_id", params.assistant_id)
-
-      let messages;
-      if (run.status === 'completed') {
-        messages = await openai.beta.threads.messages.list(
-          run.thread_id
+        // Legger til brukerens spørsmål i tråden
+        const message = await openai.beta.threads.messages.create(
+          thread.id,
+          {
+            role: "user",
+            content: params.question
+          }
         );
-        // console.log("Meldinger:", messages.data);
-        for (const message of messages.data.reverse()) {
-          console.log(`${message.role} > ${message.content[0].text.value}`);
+
+        // Kjører tråden på valgt assistent
+        const run = await openai.beta.threads.runs.createAndPoll(
+          thread.id,
+          {
+            assistant_id: params.assistant_id,
+          },
+        );
+        console.log("Ass_id", params.assistant_id)
+
+        let messages;
+        if (run.status === 'completed') {
+          messages = await openai.beta.threads.messages.list(
+            run.thread_id
+          );
+          // console.log("Meldinger:", messages.data);
+          for (const message of messages.data.reverse()) {
+            console.log(`${message.role} > ${message.content[0].text.value}`);
+          }
+        } else {
+          console.log(run.status);
         }
-      } else {
-        console.log(run.status);
-      }
 
-      let respons = {
-        run: run.status,
-        thread_id: thread.id,
-        assistant_id: run.assistant_id,
-        messages: messages.data
-      }
+        let respons = {
+          run: run.status,
+          thread_id: thread.id,
+          assistant_id: run.assistant_id,
+          messages: messages.data
+        }
 
-      return {
-        body: JSON.stringify(respons)
-      };
+        return {
+          body: JSON.stringify(respons)
+        };
+      } catch (error) {
+          return {
+            status: 401,
+            body: JSON.stringify({ error: error.message }),
+        }
+      }
     }
 });
